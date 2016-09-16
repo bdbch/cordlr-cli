f6318357c724f7e63cc4ae65464e96d5b517e0b3
Jamen Marz <jamenmarz@gmail.com>
2016-09-15 18:08:40 -0600

    Rename actions to scripts and remove from core

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const spawnargs = require('spawn-args')
 const requireGlob = require('require-glob')
 const path = require('path')
 const log = require('log-cb')
+const values = require('object-values')
 
 /**
  * Create the cordlr bot.
@@ -13,35 +14,47 @@ const log = require('log-cb')
 function create (config = {}) {
   // Create bot.
   const bot = new DiscordClient(config.client)
-  const actionsGlob = config.actions
+  const scriptGlobs = config.scripts
   const prefix = config.prefix
-  const base = path.dirname(config.config)
 
+  // Where `.cordlrrc` is, otherwise where you ran `cordlr`
+  const base = path.dirname(config.config || process.cwd())
+
+  // Bot script handling (where the magic happens)
   bot.once('ready', () => {
-    // Require the actions provided
-    requireGlob(actionsGlob, { cwd: base }).then(actions => {
-      const actionNames = Object.keys(actions)
+    if (!scriptGlobs.length) return bot.emit('done', false)
 
-      // Handle messages to run actions
+    // Require the scripts provided
+    requireGlob(scriptGlobs, { cwd: base }).then(modules => {
+      const scripts = values(modules)
+
+      // Initialize all scripts and stash command scripts
+      const commands = new Map()
+      scripts.forEach(script => {
+        const handler = script(bot, config)
+        if (script.command && handler) {
+          commands.set(script.command, handler)
+        }
+      })
+
+      // Handle messages that run command scripts
       bot.on('message', message => {
         if (!message.content.indexOf(prefix) && message.channel.type !== 'dm') {
-          // Parse the name and arguments.
+          // Parse the name and input
           const raw = message.content.slice(prefix.length)
           const args = spawnargs(raw, { removequotes: 'always' })
-          const flags = minimist(args)
-          const name = args.shift()
+          const command = args.shift()
 
-          // Run it, if it exists.
-          if (actionNames.indexOf(name) > -1) {
-            const action = actions[name](bot, config)
-            if (action) action(message, args, flags)
+          // Run it, if it is valid
+          if (commands.has(command)) {
+            commands.get(command)(message, args, minimist(args))
           }
         }
       })
 
       // Emit "done" for setup complete
-      bot.emit('done')
-    }, log.err())
+      bot.emit('done', true)
+    }).catch(log.err())
   })
 
   // Attempt to log the bot in after handler set
