ad51e518daff62a56fd4d0019fa2ef8b06154e8c
Jamen Marz <jamenmarz+gh@gmail.com>
2016-09-18 00:00:37 -0600

    Fix help flag

--- bin/methods/help.js
+++ bin/methods/help.js
@@ -2,6 +2,6 @@ const fs = require('fs')
 const path = require('path')
 
 module.exports = function () {
-  console.log(fs.readFileSync(path.join(__dirname, '../usage.txt'), 'utf-8'))
+  console.log(fs.readFileSync(path.join(__dirname, '../usage.txt')).toString('utf-8'))
   process.exit(0)
 }
