6f3f142d9c5aa3584f120f45e8f6cdafb7b22039
Mees van Dijk <mees@itisme.es>
2016-10-07 12:40:10 +0200

    removed fsproxy. config.js now respects cli args

--- bin/config.js
+++ bin/config.js
@@ -1,11 +1,31 @@
-const fsproxy = require('fs-proxy')
+const { writeFile } = require('fs')
+const configDefaults = require('./files/configDefaults.json')
+const minimist = require('minimist')
 
 const configPath = `${process.cwd()}/package.json`
 let config
 try {
-  config = fsproxy(configPath)
+  config = require(configPath)
 } catch (e) {
-  config = { cordlr: {} }
+  // file doesn't exist
+  config = { cordlr: configDefaults }
+}
+
+// insert command line options
+const args = minimist(process.argv.slice(2))
+for (const key of Object.keys(args)) {
+  if (key === '_') continue
+  config[key] = args[key]
+}
+// attach method to write config to file
+config.writeToFile = () => {
+  const writeString = JSON.stringify(config, null, 2)
+  return new Promise((resolve, reject) => {
+    writeFile(configPath, writeString, (err) => {
+      if (err) return reject(err)
+      resolve()
+    })
+  })
 }
 
 module.exports = config
