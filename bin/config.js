8f378e94ed954de5cc7e569c397e9a5de967bc14
Mees van Dijk <mees@itisme.es>
2016-10-07 11:03:53 +0200

    no functional change, forgot to remove excessive parameter

--- bin/config.js
+++ bin/config.js
@@ -3,7 +3,7 @@ const fsproxy = require('fs-proxy')
 const configPath = `${process.cwd()}/package.json`
 let config
 try {
-  config = fsproxy(configPath, (key) => key.slice(0, 2) !== ['cordlr', 'package'])
+  config = fsproxy(configPath)
 } catch (e) {
   config = { cordlr: {} }
 }
