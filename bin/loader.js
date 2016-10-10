e3b09ed252d4f9cd7ba2bee582eff99cb5ef0c6a
Jamen Marz <jamenmarz@gmail.com>
2016-10-10 01:28:06 -0600

    Add loaders and remove actions.

---
+++ bin/loader.js
@@ -0,0 +1,64 @@
+module.exports = loader
+const resolve = require('resolve')
+const minimist = require('minimist')
+const spawnargs = require('spawn-args')
+
+/**
+ * Default cordlr loader.
+ * Set "loader" in config to change
+ */
+
+function loader (bot, config) {
+  // Get plugin path(s)
+  let pluginPaths = config.plugins || []
+  if (!Array.isArray(pluginPaths)) pluginPaths = [pluginPaths]
+
+  // Where you run "cordlr"
+  const resolveOpts = { basedir: process.cwd() }
+
+  // Prefix
+  const prefix = config.prefix || '$'
+
+  // Log in using token or email/pass
+  bot.login(config.token || config.email, config.password)
+
+  return function ready () {
+    // Get plugins from paths
+    const plugins = pluginPaths.reduce(function (plugins, pluginPath) {
+      const plugin = require(resolve.sync(pluginPath, resolveOpts))
+
+      if (!Array.isArray(plugin)) plugins.push(plugin)
+      else for (const subplugin of plugin) plugins.push(subplugin)
+
+      return plugins
+    }, [])
+
+    // Initialize all plugins and stash command plugins
+    const commands = new Map()
+    plugins.forEach(plugin => {
+      const handler = plugin(bot, config)
+      if (plugin.command && handler) {
+        commands.set(plugin.command, handler)
+      }
+    })
+
+    // Handle messages that run command plugins
+    bot.on('message', message => {
+      if (!message.content.indexOf(prefix) && message.channel.type !== 'dm') {
+        // Parse the name and input
+        const input = spawnargs(message.content.slice(prefix.length))
+        const flags = minimist(input)
+        const args = flags._
+        const command = args.shift()
+
+        // Run it, if it is valid
+        if (commands.has(command)) {
+          commands.get(command)(message, args, flags)
+        }
+      }
+    })
+
+    // Emit "loaded" for setup complete
+    bot.emit('loaded', !!plugins.length || new Error('No plugins'))
+  }
+}
--- bin/methods/start.js
+++ bin/methods/start.js
@@ -1,12 +1,26 @@
 module.exports = start
+const { writeFile } = require('fs')
+const path = require('path')
 const cordlr = require('../../')
-const config = require('../config')
 const log = require('log-cb')
 
-function start () {
+function start (flags, folder = '') {
+  // Load configuration file
+  let config = {}
+  const configPath = path.resolve(process.cwd(), folder, 'package.json')
+  try { config = require(configPath) } catch (e) {}
+
+  // Add command-line options to config
+  Object.assign(config, flags)
+  delete config._
+
+  // Attach method to write config to file
+  config.writeToFile = (cb) => writeFile(configPath, JSON.stringify(config, null, 2), cb)
+
+  // Set defaults
+  if (!config.loader) config.loader = path.join(__dirname, '..', 'loader')
+
   // Create bot.
   const bot = cordlr(config)
-
-  // Log done message.
-  bot.on('done', log('Loaded bot', 'Loaded bot (with no plugins)'))
+  bot.on('loaded', log('Loaded bot', 'Loaded bot (with no plugins)'))
 }
