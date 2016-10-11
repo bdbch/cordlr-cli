9a31ce346607925876617fef51d387aed101d88b
Mees van Dijk <mees@itisme.es>
2016-10-11 11:42:25 +0200

    writeToFile now selects property and returns promise

--- bin/methods/start.js
+++ bin/methods/start.js
@@ -9,13 +9,20 @@ function start (flags, folder = '') {
   let config = {}
   const configPath = path.resolve(process.cwd(), folder, 'package.json')
   try { config = require(configPath) } catch (e) {}
-
+  const uneditedConfig = Object.assign({}, config)
   // Add command-line options to config
   Object.assign(config, flags)
   delete config._
 
   // Attach method to write config to file
-  config.writeToFile = (cb) => writeFile(configPath, JSON.stringify(config, null, 2), cb)
+  config.writeToFile = (property) => new Promise((resolve, reject) => {
+    if (!config[property]) return reject(new Error(`error writing property ${property} to config file, no such property`))
+    uneditedConfig[property] = config[property]
+    writeFile(configPath, JSON.stringify(uneditedConfig, null, 2), (err) => {
+      if (err) reject(err)
+      else resolve()
+    })
+  })
 
   // Set defaults
   if (!config.loader) config.loader = path.join(__dirname, '..', 'loader')
