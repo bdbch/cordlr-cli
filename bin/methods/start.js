23b873682e9e7efbed68a8413f488bbbc14850c1
Mees van Dijk <mees@itisme.es>
2016-10-13 15:55:10 +0200

    added my proposed changes

--- bin/methods/start.js
+++ bin/methods/start.js
@@ -1,23 +1,33 @@
 module.exports = start
 const { writeFile } = require('fs')
-const path = require('path')
+const { resolve } = require('path')
 const log = require('log-cb')
 const { Client } = require('discord.js')
 
-function start (flags, plugins = []) {
+function start (flags) {
   // Load configuration file
   let config = {}
-  const configPath = path.resolve(process.cwd(), 'package.json')
+  const configPath = resolve(process.cwd(), 'package.json')
   try {
     config = require(configPath)
   } catch (e) {
     if (e.code !== 'MODULE_NOT_FOUND') return log()(e)
   }
 
+  // Set defaults
+  if (!config.loader) config.loader = 'cordlr-loader'
+  if (!config.plugins) config.plugins = []
+
   // Add command-line options to config
   const uneditedConfig = Object.assign({}, config)
   Object.assign(config, flags)
   delete config._
+  // add command-line plugins to config (if they weren't there yet)
+  for (const plugin of flags._) {
+    if (!config.plugins.includes(plugin)) {
+      config.plugins.push(plugin)
+    }
+  }
 
   // Attach method to write config to file
   config.writeToFile = (property) => new Promise((resolve, reject) => {
@@ -29,16 +39,8 @@ function start (flags, plugins = []) {
     })
   })
 
-  // Set defaults
-  if (!config.loader) config.loader = path.join(__dirname, '../../node_modules/cordlr-loader')
-  if (plugins.length) {
-    if (!config.plugins) config.plugins = []
-    config.plugins.push(...plugins)
-  }
-
   // Create bot
   const bot = new Client()
-  bot.on('error', log.err())
   bot.on('ready', log('Loaded successfully'))
 
   // Load loader
