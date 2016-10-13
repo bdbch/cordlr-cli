f292ae48a6606a798619e4c3898a506e327bdf5e
Jamen Marz <jamenmarz@gmail.com>
2016-10-13 03:10:00 -0600

    Fix errors (Closes #86 and #85)

--- bin/methods/start.js
+++ bin/methods/start.js
@@ -1,23 +1,27 @@
 module.exports = start
 const { writeFile } = require('fs')
 const path = require('path')
-const cordlr = require('../../')
 const log = require('log-cb')
+const { Client } = require('discord.js')
 
 function start (flags, plugins = []) {
   // Load configuration file
   let config = {}
   const configPath = path.resolve(process.cwd(), 'package.json')
-  try { config = require(configPath) } catch (e) { return log()(e) }
-  const uneditedConfig = Object.assign({}, config)
-  // Add command-line options to config
+  try {
+    config = require(configPath)
+  } catch (e) {
+    if (e.code !== 'MODULE_NOT_FOUND') return log()(e)
+  }
 
+  // Add command-line options to config
+  const uneditedConfig = Object.assign({}, config)
   Object.assign(config, flags)
   delete config._
 
   // Attach method to write config to file
   config.writeToFile = (property) => new Promise((resolve, reject) => {
-    if (!config[property]) return reject(new Error(`error writing property ${property} to config file, no such property`))
+    if (!config[property]) return reject(new Error(`Cannot write property "${property}"`))
     uneditedConfig[property] = config[property]
     writeFile(configPath, JSON.stringify(uneditedConfig, null, 2), (err) => {
       if (err) reject(err)
@@ -26,14 +30,18 @@ function start (flags, plugins = []) {
   })
 
   // Set defaults
-  if (!config.loader) config.loader = path.join(__dirname, '..', 'loader')
+  if (!config.loader) config.loader = path.join(__dirname, '../../node_modules/cordlr-loader')
   if (plugins.length) {
     if (!config.plugins) config.plugins = []
     config.plugins.push(...plugins)
   }
 
-  // Create bot.
-  const bot = cordlr(config)
+  // Create bot
+  const bot = new Client()
   bot.on('error', log.err())
-  bot.on('loaded', log('Loaded successfully'))
+  bot.on('ready', () => log('Loaded successfully')())
+
+  // Load loader
+  const loader = require(config.loader)
+  loader(bot, config)
 }
