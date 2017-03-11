3cc6d88c17b244dcd2aadd1c2ef38272810ebc73
AlphaStyle <torbjorn1991@hotmail.com>
2017-03-11 12:34:50 +0100

    quick save before changing branch

--- bin/methods/help.js
+++ bin/methods/help.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
 const path = require('path')
 
-const USAGE_PATH = path.join(__dirname, '../files/usage.txt')
-const USAGE = fs.readFileSync(USAGE_PATH).toString()
-
 module.exports = class Help {
   constructor () {
+    const USAGE = fs.readFileSync(USAGE_PATH).toString()
+    const USAGE_PATH = path.join(__dirname, '../files/usage.txt')
+    
     console.log(USAGE)
   }
 }
