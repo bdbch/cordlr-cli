a38c51ace168ab6ffd093e022214315674805269
AlphaStyle <torbjorn1991@hotmail.com>
2017-03-11 12:44:35 +0100

    small cleanups and added comments

--- bin/methods/help.js
+++ bin/methods/help.js
@@ -3,9 +3,9 @@ const path = require('path')
 
 module.exports = class Help {
   constructor () {
-    const USAGE = fs.readFileSync(USAGE_PATH).toString()
     const USAGE_PATH = path.join(__dirname, '../files/usage.txt')
-    
+    const USAGE = fs.readFileSync(USAGE_PATH).toString()
+
     console.log(USAGE)
   }
 }
