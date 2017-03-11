e856b9f1c686fe596e7111417402061bf0a1d190
db2k <dominik.biedebach@gmail.com>
2017-03-11 02:07:38 +0100

    Fixed build errors

--- bin/methods/start.js
+++ bin/methods/start.js
@@ -1,10 +1,9 @@
 const { Client } = require('discord.js')
 const path = require('path')
-const { writeFile } = require('fs')
 
 module.exports = class Cordlr {
-  constructor(flags) { 
-    this.flags = flags || ""
+  constructor (flags) { 
+    this.flags = flags || ''
 
     // Get path to "cordlr config" file
     this.configPath = path.resolve(process.cwd(), 'cordlr.json')
@@ -15,7 +14,7 @@ module.exports = class Cordlr {
       if (e.code === 'MODULE_NOT_FOUND') this.config = {}
       else return console.log(e)
     }
-  
+
     // If Token doesn't exist, return error
     if (!this.config.token) return console.log('No token specified')
 
@@ -23,7 +22,7 @@ module.exports = class Cordlr {
     if (!this.config.loader) this.config.loader = this.flags.loader || 'cordlr-loader'
     if (!this.config.plugins) this.config.plugins = this.flags.plugins || []
     if (!this.config.prefix) this.config.prefix = this.flags.prefix || '!'
-    
+
     // Add plugin(s) from command-line flags to "cordlr config" file
     if (this.flags.plugins) {
       for (const plugin of this.flags.plugins) {
@@ -36,14 +35,14 @@ module.exports = class Cordlr {
     this.start()
   }
 
-  start() {
+  start () {
     // Initialize and load the bot and loader
     const bot = new Client()
     bot.on('error', (e) => console.log(e))
     bot.on('ready', () => console.log('Loaded successfully'))
 
     // Load loader
-    const loader = require(this.config.loader)
-    const l = new loader(bot, this.config)
+    const Loader = require(this.config.loader)
+    return new Loader(bot, this.config)
   }
 }
